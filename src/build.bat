5c138e916e3d67324372fe588aab964649a32f23
Thomas Freudenberg <info@thomasfreudenberg.com>
2014-12-30 23:19:34 +0100

    f*ck

--- src/build.bat
+++ src/build.bat
@@ -1,1 +1,3 @@
+%NuGet% restore %SourcesPath%\src\TinyLittleMvvm.sln -PackagesDirectory %SourcesPath%\src\packages\
+
 "%MsBuildExe%" %SourcesPath%\src\TinyLittleMvvm.sln /t:%Targets% /p:Configuration=%Configuration%
