2e37b9c848edcd7e1110688939905d690a0bfef9
Thomas Freudenberg <info@thomasfreudenberg.com>
2015-01-11 19:08:26 +0100

    documented ValidationPropertyChangedBase

--- src/TinyLittleMvvm/ValidationPropertyChangedBase.cs
+++ src/TinyLittleMvvm/ValidationPropertyChangedBase.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using System.Linq.Expressions;
 
 namespace TinyLittleMvvm {
+    /// <summary>
+    /// A base class for <see cref="DialogViewModel"/> providing validation.
+    /// </summary>
     public abstract class ValidationPropertyChangedBase : PropertyChangedBase, INotifyDataErrorInfo {
         private readonly Dictionary<string, List<string>> _errors = new Dictionary<string, List<string>>(StringComparer.OrdinalIgnoreCase);
         private readonly Dictionary<string, List<ValidationRule>> _validationRules = new Dictionary<string, List<ValidationRule>>();
@@ -15,6 +18,13 @@ namespace TinyLittleMvvm {
             public Func<bool> IsValid { get; set; }
         }
 
+        /// <summary>
+        /// Adds a rule for validation.
+        /// </summary>
+        /// <param name="property">The property to validate.</param>
+        /// <param name="validate">The function to validate the value of the property.</param>
+        /// <param name="errorMsg">The error message if the validation fails.</param>
+        /// <typeparam name="TProperty">The type of the property to validate.</typeparam>
         protected void AddValidationRule<TProperty>(Expression<Func<TProperty>> property, Func<TProperty, bool> validate, string errorMsg) {
             var propertyName = ExpressionHelper.GetMemberName(property);
 
@@ -34,6 +44,14 @@ namespace TinyLittleMvvm {
             ruleSet.Add(rule);
         }
 
+        /// <summary>
+        /// Gets the validation errors for a specified property or for the entire entity.
+        /// </summary>
+        /// <returns>
+        /// The validation errors for the property or entity.
+        /// </returns>
+        /// <param name="propertyName">The name of the property to retrieve validation errors for;
+        /// or null or <see cref="F:System.String.Empty"/>, to retrieve entity-level errors.</param>
         public IEnumerable GetErrors(string propertyName) {
             if (!String.IsNullOrEmpty(propertyName)) {
                 List<string> propertyErrors;
@@ -43,6 +61,10 @@ namespace TinyLittleMvvm {
             }
         }
 
+        /// <summary>
+        /// Raises the <see cref="PropertyChangedBase.PropertyChanged"/> event.
+        /// </summary>
+        /// <param name="propertyName">The name of the changed property.</param>
         protected override void NotifyOfPropertyChange(string propertyName = null) {
             base.NotifyOfPropertyChange(propertyName);
 
@@ -62,6 +84,10 @@ namespace TinyLittleMvvm {
             }
         }
 
+        /// <summary>
+        /// Runs all validation rules.
+        /// </summary>
+        /// <returns><see langword="true"/> if all rules are valid; otherwise, <see langword="false"/>.</returns>
         protected bool ValidateAllRules() {
             foreach (var ruleSet in _validationRules) {
                 foreach (var rule in ruleSet.Value) {
@@ -75,12 +101,25 @@ namespace TinyLittleMvvm {
             return !HasErrors;
         }
 
+        /// <summary>
+        /// Gets a value that indicates whether the entity has validation errors.
+        /// </summary>
+        /// <returns>
+        /// true if the entity currently has validation errors; otherwise, false.
+        /// </returns>
         public bool HasErrors {
             get { return _errors.Any(); }
         }
 
+        /// <summary>
+        /// Occurs when the validation errors have changed for a property or for the entire entity.
+        /// </summary>
         public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
 
+        /// <summary>
+        /// Called when the validation state of a property has changed.
+        /// </summary>
+        /// <param name="propertyName">The name of the validated property.</param>
         protected virtual void OnErrorsChanged(string propertyName) {
             var handler = ErrorsChanged;
             if (handler != null) {
@@ -88,10 +127,23 @@ namespace TinyLittleMvvm {
             }
         }
 
+        /// <summary>
+        /// Sets a property's error state.
+        /// </summary>
+        /// <param name="isValid"><see langword="true"/> if the property is valid; otherwise, <see langword="false"/>.</param>
+        /// <param name="property">The validated property.</param>
+        /// <param name="error">The error message.</param>
+        /// <typeparam name="TProperty">The type of the property.</typeparam>
         protected void Validate<TProperty>(bool isValid, Expression<Func<TProperty>> property, string error) {
             Validate(isValid, ExpressionHelper.GetMemberName(property), error);
         }
 
+        /// <summary>
+        /// Sets a property's error state.
+        /// </summary>
+        /// <param name="isValid"><see langword="true"/> if the property is valid; otherwise, <see langword="false"/>.</param>
+        /// <param name="propertyName">The name of the validated property.</param>
+        /// <param name="error">The error message.</param>
         protected void Validate(bool isValid, string propertyName, string error) {
             if (isValid) {
                 RemoveError(propertyName, error);
@@ -100,10 +152,21 @@ namespace TinyLittleMvvm {
             }
         }
 
+        /// <summary>
+        /// Adds a validation error message for a property.
+        /// </summary>
+        /// <param name="property">The validated property.</param>
+        /// <param name="error">The error message.</param>
+        /// <typeparam name="TProperty">The type of the validated property.</typeparam>
         protected void AddError<TProperty>(Expression<Func<TProperty>> property, string error) {
             AddError(ExpressionHelper.GetMemberName(property), error);
         }
 
+        /// <summary>
+        /// Adds a validation error message for a property.
+        /// </summary>
+        /// <param name="propertyName">The name of the validated property.</param>
+        /// <param name="error">The error message.</param>
         protected void AddError(string propertyName, string error) {
             List<string> propertyErrors;
             if (!_errors.TryGetValue(propertyName, out propertyErrors)) {
@@ -116,10 +179,21 @@ namespace TinyLittleMvvm {
             }
         }
 
+        /// <summary>
+        /// Removes a validation error message for a property.
+        /// </summary>
+        /// <param name="property">The validated property.</param>
+        /// <param name="error">The error message.</param>
+        /// <typeparam name="TProperty">The type of the validated property.</typeparam>
         protected void RemoveError<TProperty>(Expression<Func<TProperty>> property, string error) {
             RemoveError(ExpressionHelper.GetMemberName(property), error);
         }
 
+        /// <summary>
+        /// Removes a validation error message for a property.
+        /// </summary>
+        /// <param name="propertyName">The name of the validated property.</param>
+        /// <param name="error">The error message.</param>
         protected void RemoveError(string propertyName, string error) {
             List<string> propertyErrors;
             if (_errors.TryGetValue(propertyName, out propertyErrors)) {
