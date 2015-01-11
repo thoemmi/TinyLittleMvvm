267e97d35c7123a2308de9c9963768e92c75a591
Thomas Freudenberg <info@thomasfreudenberg.com>
2015-01-11 18:25:33 +0100

    documented IOnXxxHandlers

--- src/TinyLittleMvvm/IOnClosingHandler.cs
+++ src/TinyLittleMvvm/IOnClosingHandler.cs
@@ -1,5 +1,15 @@
+using System.Windows;
+
 namespace TinyLittleMvvm {
+    /// <summary>
+    /// This interface can be implemented by view models, which want to be notified when
+    /// the corresponding view is about to be closed.
+    /// </summary>
     public interface IOnClosingHandler {
+        /// <summary>
+        /// This method is called when the corresponding view's <see cref="FrameworkElement.Loaded"/> event was raised.
+        /// </summary>
+        /// <returns></returns>
         void OnClosing();
     }
 }
--- src/TinyLittleMvvm/IOnLoadedHandler.cs
+++ src/TinyLittleMvvm/IOnLoadedHandler.cs
@@ -1,7 +1,17 @@
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace TinyLittleMvvm {
+    /// <summary>
+    /// This interface can be implemented by view models, which want to be notified when
+    /// the corresponding view was loaded.
+    /// </summary>
     public interface IOnLoadedHandler {
+        /// <summary>
+        /// This method is called when the corresponding view's <see cref="Window.Closing"/> or
+        /// <see cref="FrameworkElement.Unloaded"/> event was raised.
+        /// </summary>
+        /// <returns></returns>
         Task OnLoadedAsync();
     }
 }
