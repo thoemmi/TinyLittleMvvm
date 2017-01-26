ad09fc011335b22cda43ccdd5b3360248759f2e2
Thomas Freudenberg <info@thomasfreudenberg.com>
2017-01-26 22:21:41 +0100

    added base class and theme for wizard pages

--- src/TinyLittleMvvm/Properties/AssemblyInfo.cs
+++ src/TinyLittleMvvm/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Windows;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -20,3 +21,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5abee8dd-260f-49dd-94ad-94ddb143e9f9")]
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
---
+++ src/TinyLittleMvvm/Wizard/Views/WizardPageView.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TinyLittleMvvm.Wizard.Views {
+    /// <summary>
+    /// Optional base class for wizard pages.
+    /// </summary>
+    public class WizardPageView : UserControl {
+        static WizardPageView() {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WizardPageView), new FrameworkPropertyMetadata(typeof(WizardPageView)));
+        }
+
+        /// <summary>
+        /// The <see cref="DependencyProperty"/> for the <see cref="Header"/> property.
+        /// </summary>
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register(
+            "Header", typeof(string), typeof(WizardPageView), new PropertyMetadata(default(string)));
+
+        /// <summary>
+        /// Gets or sets the header.
+        /// </summary>
+        public string Header {
+            get { return (string) GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        /// <summary>
+        /// The <see cref="DependencyProperty"/> for the <see cref="Subheader"/> property.
+        /// </summary>
+        public static readonly DependencyProperty SubheaderProperty = DependencyProperty.Register(
+            "Subheader", typeof(string), typeof(WizardPageView), new PropertyMetadata(default(string)));
+
+        /// <summary>
+        /// The subheader.
+        /// </summary>
+        public string Subheader {
+            get { return (string) GetValue(SubheaderProperty); }
+            set { SetValue(SubheaderProperty, value); }
+        }
+    }
+}
