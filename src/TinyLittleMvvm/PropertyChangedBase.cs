7c1d6d212b7eda14a6f7f594db2d81d1a5ecac08
Jan Karger <punkerat76@gmail.com>
2018-01-11 21:59:22 +0100

    Add Set<T> method   which fully implements a Setter for a read-write property, using CallerMemberName to raise the notification and the ref to the backing field to set the property.

--- src/TinyLittleMvvm/PropertyChangedBase.cs
+++ src/TinyLittleMvvm/PropertyChangedBase.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
 namespace TinyLittleMvvm {
@@ -12,6 +14,26 @@ namespace TinyLittleMvvm {
         /// Occurs when a property value changes.
         /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Set fully implements a Setter for a read-write property,
+        /// using CallerMemberName to raise the notification
+        /// and the ref to the backing field to set the property.
+        /// </summary>
+        /// <typeparam name="T">The type of the return value.</typeparam>
+        /// <param name="backingField">A Reference to the backing field for this property.</param>
+        /// <param name="newValue">The new value.</param>
+        /// <param name="propertyName">The name of the property, usually automatically provided through the CallerMemberName attribute.</param>
+        /// <returns>True if the PropertyChanged event was raised, false otherwise.</returns> 
+        public bool Set<T>(ref T backingField, T newValue, [CallerMemberName] string propertyName = null) {
+            if (EqualityComparer<T>.Default.Equals(backingField, newValue)) {
+                return false;
+            }
+
+            backingField = newValue;
+            NotifyOfPropertyChange(propertyName);
+            return true;
+        }
 
         /// <summary>
         /// Notifies clients that all properties may have changed.
@@ -28,7 +50,7 @@ namespace TinyLittleMvvm {
         /// Raises the <see cref="PropertyChanged"/> event.
         /// </summary>
         /// <param name="propertyName">The name of the changed property.</param>
-        protected virtual void NotifyOfPropertyChange(string propertyName = null) {
+        protected virtual void NotifyOfPropertyChange([CallerMemberName] string propertyName = null) {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
