640be8f2b03ac84260c75c466f9a566367df47e5
Thomas Freudenberg <info@thomasfreudenberg.com>
2018-02-07 01:19:23 +0100

    Updated references packages and appveyor (#9)
    * Fixed referencing and packages
    * update solution to VS2017
    * converted projects to new SDK format too
    * publish nuget package only when on master and develop

--- src/pre-build.bat
+++
@@ -1,3 +0,0 @@
-%NuGet% install GitVersion.CommandLine -version 3.4.1 -NonInteractive -o %SourcesPath%\src\packages\
-
-%SourcesPath%\src\packages\GitVersion.CommandLine.3.4.1\Tools\GitVersion.exe /output buildserver
