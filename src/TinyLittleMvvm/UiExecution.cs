013ebc302053408e25ec5637ef66a1de938a22a0
Thomas Freudenberg <info@thomasfreudenberg.com>
2021-10-11 18:23:21 +0200

    Application.Current.MainWindow throws if accessed from non-UI thread

--- src/TinyLittleMvvm/UiExecution.cs
+++ src/TinyLittleMvvm/UiExecution.cs
@@ -6,12 +6,12 @@ using System.Windows.Threading;
 namespace TinyLittleMvvm {
     internal class UiExecution : IUiExecution {
         public void Execute(Action action) {
-            var dispatcher = Application.Current.MainWindow?.Dispatcher ?? Dispatcher.CurrentDispatcher;
+            var dispatcher = Application.Current.Dispatcher ?? Dispatcher.CurrentDispatcher;
             dispatcher.Invoke(action);
         }
 
         public Task ExecuteAsync(Action action) {
-            var dispatcher = Application.Current.MainWindow?.Dispatcher ?? Dispatcher.CurrentDispatcher;
+            var dispatcher = Application.Current.Dispatcher ?? Dispatcher.CurrentDispatcher;
             return dispatcher.InvokeAsync(action).Task;
         }
     }
